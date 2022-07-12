cb8303698fa39514bdf42210d1a1d99eb77ffab5
dojoohwan <ehwnghks@gmail.com>
2022-07-13 01:18:41 +0900

    spring boot 실행 test

---
+++ src/main/java/com/dojoohwan/springbootes/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.dojoohwan.springbootes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Hello, World!";
+    }
+}
+
