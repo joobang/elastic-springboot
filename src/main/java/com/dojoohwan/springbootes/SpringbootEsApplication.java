b522b98386bc293be3e74dc2116d01f5f9d1d26a
dojoohwan <ehwnghks@gmail.com>
2022-07-16 19:04:07 +0900

    실행 오류 수정

--- src/main/java/com/dojoohwan/springbootes/SpringbootEsApplication.java
+++ src/main/java/com/dojoohwan/springbootes/SpringbootEsApplication.java
@@ -2,8 +2,9 @@ package com.dojoohwan.springbootes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringbootEsApplication {
 
 	public static void main(String[] args) {
